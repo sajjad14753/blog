781fa82af25f5acc93733c39cf4a61183fd4ba4d
sajjad14753 <sajjad.alimohamadi97@gmail.com>
2023-06-09 19:11:54 +0330

    start faq

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request,'homepage.html', {})
+def faq(request):
+    return render(request,'faq.html', {})
