227f467d34208e3becb2c93e0874700c002cc415
sajjad14753 <sajjad.alimohamadi97@gmail.com>
2023-06-25 18:34:34 +0330

    new post, post-dstails

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('faq/', views.faq, name='faq' ),
+    path('post/new/',views.new_post, name='new_post'),
+    path('post/<pk>/',views.post_details, name='post_details'),   
 ]
